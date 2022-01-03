a6afed9f84d3744fb20e103eb5e9c37cb0332304
thiam <lmtlibasse@saloumpharma.com>
2022-01-03 00:43:06 +0000

    avant routing

--- src/app.js
+++ src/app.js
@@ -1,149 +1,220 @@
 
-
     const API_URL = "https://bhuprmvnemqneqrwprpn.supabase.co/rest/v1/school_app"
     const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTMwNTgxMiwiZXhwIjoxOTU0ODgxODEyfQ.qju7skRPAGkGzrag_7KX25VBqwdFhCEs94dLFfGEJcw" 
 
-var donnes = []
-
-const form = document.getElementById("Myform")
-const nom = document.querySelector("input#nom")
-const prenom = document.querySelector("input#prenom")
-const biographie = document.querySelector("textarea#bio")
-const niveau = document.querySelector("select#niveau")
-const sauvegarder = document.getElementById("sauvegarder")
-const afficheApprenant = document.querySelector(".affiche-apprenant")
-
-biographie.addEventListener('input', (e) => {
-    let textBio = document.getElementById("PBio")
-    document.querySelector('#longeurText').innerHTML = biographie.value.length
-    biographie.maxLength = 130;
-    if (biographie.value.length === 130) {
-        biographie.style.border = "5px solid red"
-        textBio.style.color = "red"
-    } else {
-        biographie.style.border = "solid black"
-        textBio.style.color = "black"
-    }
-})
-
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    let nouveauapprenants = {
-        prenom : prenom.value,
-        nom : nom.value,
-        bio : biographie.value,
-        niveau : niveau.value
-    }
-    apprenantAffiche(nouveauapprenants.nom, nouveauapprenants.prenom, nouveauapprenants.bio, nouveauapprenants.niveau)
-    donnes.push(nouveauapprenants)
-   modifierChamp()
-   deletecarte()
-})
 
 
-function apprenantAffiche(nom, prenom, bio, niveau){
-    afficheApprenant.insertAdjacentHTML(
-        "beforeend",
-        `<div class="card" style="width: 18rem;">
-        <img src="/img/ex.png" class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title"><span>${nom}</span><span>${prenom}</span></h5>
-        <p class="card-text">${bio}</p>
-        <p class="niveau card-text">${niveau}</p>
-        <div class="card-icone d-flex flex-row  justify-content-between">
-        <a href="#" class="btn btn-outline-success" id="modifier"><i class="bi bi-pen"></i></a>
-        <a href="#" class="btn btn-outline-danger" id="annuler"><i class="bi bi-x-circle"></i></a>
-        </div>
-        </div>
-    </div>`
-    )  
-}    
-function modifierChamp(){
-    const modifier = document.getElementById("modifier")
-    modifier.addEventListener('click', (e)=>{
-        e.preventDefault()
-    let nomModifier = document.createElement('input')
-    nomModifier.setAttribute("placeholder", "modifier prenom & nom*")
-    let baliseh5 = document.querySelector(".card-title")
-    let balisep = document.querySelector(".card-text")
-    let bioModifier = document.createElement('textarea')
-    
-
-    let carteModifier = document.querySelector(".card-body")
-
-    carteModifier.replaceChild(nomModifier, baliseh5)
-    carteModifier.replaceChild(bioModifier, balisep)
-        let carte = document.querySelector(".card")
-    
-    modifier.addEventListener('click', ()=>{
-        let nouveauNom = nomModifier.value
-        let nouveauBio = bioModifier.value   
-        apprenantAffiche(nouveauNom, "", nouveauBio)
-        carte.remove()
-    })   
-})
-    deletecarte()   
-}
-        //suppression carte
-        function deletecarte(){
-            const annule = document.getElementById("annuler")
-            annule.addEventListener('click', ()=>{
-                let carte = document.querySelector(".card")
-                carte.remove()
-            })
-        }
-        
-    //modification et suppression de la nouvelle carte 
- 
-
-    sauvegarder.addEventListener('click', (e)=>{
-        e.preventDefault()
-        fetch(API_URL, {
-                method: "POST",
-                headers: {
-                    apiKey: API_KEY,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(donnes),
-            })
-            return donnes = []
-        })
+        const btn = document.getElementById("ajouter")
+        const afficheApprenant = document.querySelector(".affiche-apprenant")
+        const sauvegarder = document.getElementById("ajouter")
 
 
-   /*  (function showData(){
-        let { data: school_app, error } = await supabase
-            .from('school_app')
-            .select('*')
+        let donnees = []
+       
+        let donnee = donnees [donnees.length - 1]
+
+        btn.addEventListener('click', (e) =>{
+            e.preventDefault()
+            let nom = document.getElementById("nom")
+            let prenom = document.getElementById("prenom")
+            let biographie = document.getElementById("bio")
+            let niveau = document.getElementById("niveau")
+            let competence1 = document.getElementById("competence1")
+            let competence2 = document.getElementById("competence2")
             
-    })() */
+           
+            let newDonnees = {
+                nom : nom.value,
+                prenom : prenom.value,
+                bio: biographie.value,
+                niveau : niveau.value,
+                competence1 : competence1.value,
+                competence2 : competence2.value
+            } 
+             donnees.push(newDonnees)
+             
+            
+            nom.value = ""
+            prenom.value = " "
+            biographie.value = " "
+            niveau.value = " "
+            competence1.value = " "
+            competence2.value =" "
+            
+           
+            apprenantAffiche(newDonnees.nom, newDonnees.prenom, newDonnees.bio, newDonnees.niveau)
+            
+            
+        })
+        
 
+/**********************Affficher donnees dans les cartes **************************** */
+
+    
+       
+        function apprenantAffiche(nom, prenom, bio, niveau, id){
+           
+            // donnees.forEach( (element,id) => {
+               
+            afficheApprenant.insertAdjacentHTML(
+                "beforeend",
+                `<div class="card" style="width: 18rem;" id = "${"carte" +""+id}" >
+                <img src="/img/ex.png" class="card-img-top" alt="..." >
+                <div class="card-body">
+                <h5 class="card-title"><span>${nom}</span><span>${prenom}</span></h5>
+                <p class="card-text">${bio}</p>
+                <p class="niveau card-text">${niveau}</p>
+                <div class="card-icone d-flex flex-row  justify-content-between">
+
+                <button href="#" class="btn btn-outline-success modifier" id = "${id}" ><i class="bi bi-pen"></i></button>
+                <button href="#" class="btn btn-outline-danger supprimmer" id = "${id}" ><i class="bi bi-x-circle"></i></button>
+                </div>
+                </div>
+            </div>`
+            )  
+            modifierChamp()
+            deletecarte()
+        
+        }    
+       
+/**************************Modifier Carte******************************/
+
+function modifierChamp(){
+    
+    const modifier = document.querySelectorAll(".modifier")
+    modifier.forEach((element) =>{
+
+    element.addEventListener('click', ()=>{
+        
+        let nomModifier = document.createElement('input')
+        nomModifier.setAttribute("placeholder", "modifier prenom & nom*")
+        let baliseh5 = document.querySelector(".card-title")
+        let balisep = document.querySelector(".card-text")
+        let bioModifier = document.createElement('textarea')
+    
+
+        let carteModifier = document.querySelectorAll(".card-body")
+        carteModifier.forEach((carte)=> {
+            carte.replaceChild(nomModifier, baliseh5)
+            carte.replaceChild(bioModifier, balisep)
+        
+            
+                element.addEventListener('click', ()=>{
+                    let nouveauNom = nomModifier.value
+                    let nouveauBio = bioModifier.value  
+                    carte.replaceChild(baliseh5,nomModifier)
+                    carte.replaceChild(balisep, bioModifier) 
+                    baliseh5.innerHTML = nouveauNom
+                    balisep.innerHTML = nouveauBio
+            
+                })   
+
+            })
+        })
+    })
+}
+
+
+/************************Supprimmer Carte********************************** */
+
+function deletecarte(){
+    const annule = document.querySelectorAll(".supprimmer")
+    annule.forEach((elmt)=>{
+        elmt.addEventListener('click', ()=>{
+            let carte = document.querySelectorAll(".card")
+            carte.forEach((card)=>{
+                card.remove()
+            })
+            
+        })
+    })   
+}
+
+/********************************Sauvegarder dans supabase************************************ */
+
+sauvegarder.addEventListener('click', ()=>{
 
     fetch(API_URL, {
-        headers: {
-          apikey: API_KEY,
-        },
-      })
-        .then((response) => response.json(),)
-        .then ((donnes) => {
-          donnes.forEach((donnee) => showListe(donnee.nom, donnee.prenom, donnee.bio, donnee.niveau))
+            method: "POST",
+            headers: {
+                apiKey: API_KEY,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(donnees),
         })
-  
-  function showListe(nom, prenom, bio, niveau){
-      const listeApprenant = document.querySelector(".Liste")   
-          listeApprenant.insertAdjacentHTML(
-              "beforeend", 
-              `<div class="card" style="width: 18rem;">
-              <img src="/img/ex.png" class="card-img-top" alt="...">
-              <div class="card-body">
-              <h5 class="card-title"><span>${nom}</span><span>${prenom}</span></h5>
-              <p class="card-text">${bio}</p>
-              <p class="niveau card-text">${niveau}</p>
-              <div class="card-icone d-flex flex-row  justify-content-between">
-              <a href="#" class="btn btn-outline-success" id="modifier"><i class="bi bi-pen"></i></a>
-              <a href="#" class="btn btn-outline-danger" id="annuler"><i class="bi bi-x-circle"></i></a>
-              </div>
-              </div>
-          </div>`
-          )   
-      }  
-  
+         return donnees = []
+})
+
+
+/*******************************Afficher les cartes enregistrées********************************************************** */
+
+fetch(API_URL, {
+    headers: {
+      apikey: API_KEY,
+    },
+})
+    .then((response) => response.json(),)
+    .then ((donnes) => {
+      donnes.forEach((donnee) => showListe(donnee.id,donnee.nom, donnee.prenom, donnee.bio, donnee.niveau))
+      
+    })
+    
+      
+    
+
+function showListe(id,nom, prenom, bio, niveau){
+  const listeApprenant = document.querySelector(".Liste")   
+      listeApprenant.insertAdjacentHTML(
+          "beforeend", 
+          `<div class="card" style="width: 18rem;">
+          <img src="/img/ex.png" class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="card-title"><span>${nom}</span><span>${prenom}</span></h5>
+          <p class="card-text">${bio}</p>
+          <p class="niveau card-text">${niveau}</p>
+          <div class="card-icone d-flex flex-row  justify-content-between">
+          <a href="#" class="btn btn-outline-success" id="${id}" onclick = "trt(${id})" >Details<i class="bi bi-eye"></i></a>
+          </div>
+          </div>
+      </div>`
+      )   
+  }  
+
+  /***************************************************************** */
+ 
+ 
+  function showdetails(nom, prenom, bio, niveau, competence1, competence2){
+    let detail = document.getElementById("detail")
+        detail.insertAdjacentHTML(
+            "beforeend",
+            `<div class="card" style="width: 18rem;">
+            <img src="/img/ex.png" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title"><span>${nom}</span><span>${prenom}</span></h5>
+            <p class="card-text">${bio}</p>
+            <p class="niveau card-text">${niveau}</p>
+            <p class="niveau card-text">${competence1}</p>
+            <p class="niveau card-text">${competence2}</p>
+            <div class="card-icone d-flex flex-row  justify-content-between">
+            <a href="#" class="btn btn-outline-success" id="${id}">Details<i class="bi bi-eye"></i></a>
+            </div>
+            </div>
+        </div>`
+        )  
+
+    }
+
+    function trt(id){
+      fetch(`${API_URL}?id=eq.${id}`, {
+        headers: {
+            "Content-Type": "application/json",
+            apiKey: API_KEY,
+            Prefer: "return=representation",
+        },
+    })
+        .then((response) => response.json())
+        // .then((donnee) => showdetails(donnee.id: donnee.nom, donnee.prenom, donnee.bio, donnee.niveau, donnee.competence1, donnee.competence2));
+
+
+    }
+   
